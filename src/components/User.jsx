2947ea0a4a5291651deacc4957246697544ee404
Ryan Rianto <inikotaksampah@gmail.com>
2024-05-26 22:41:14 +0700

    feat: add pagging and improve styling user page

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,45 +2,74 @@ import { useEffect, useState } from "react";
 import { getUsers } from "../services/api";
 import { Link } from "react-router-dom";
 import "../assets/styles/Dashboard.css";
+import ReactPaginate from "react-paginate";
 
-const UserPage = () => {
-  //libs
+const UsersPage = () => {
   const [users, setUsers] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const usersPerPage = 5; // Jumlah pengguna per halaman
 
   useEffect(() => {
     getUsers().then((response) => setUsers(response.data));
   }, []);
 
-  console.log(users);
+  const pageCount = Math.ceil(users.length / usersPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  const displayUsers = users
+    .slice(pageNumber * usersPerPage, (pageNumber + 1) * usersPerPage)
+    .map((user) => (
+      <tr key={user.id}>
+        <td>{user.name}</td>
+        <td>
+          <button className="action-button">
+            <Link to={`/posts/${user.id}`} className="button-link">
+              <i className="fas fa-newspaper"></i>Posts
+            </Link>
+          </button>
+        </td>
+        <td>
+          <button className="action-button">
+            <Link to={`/albums/${user.id}`} className="button-link">
+              <i className="fas fa-images"></i>Albums
+            </Link>
+          </button>
+        </td>
+      </tr>
+    ));
 
   return (
     <div className="admin-dashboard">
       <div className="admin-container">
         <main className="admin-main">
-          <h2>Users</h2>
+          <h2>List Of Users</h2>
           {users.length === 0 ? (
             <p>No users found.</p>
           ) : (
-            <table className="user-table">
-              <thead>
-                <th>Name</th>
-                <th>Posts</th>
-                <th>Albums</th>
-              </thead>
-              <tbody>
-                {users.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.name}</td>
-                    <td>
-                      <Link to={`/posts/${user.id}`}>View Posts</Link>
-                    </td>
-                    <td>
-                      <Link to={`/albums/${user.id}`}>View Albums</Link>
-                    </td>
+            <div>
+              <table className="user-table">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th width="10%">Posts</th>
+                    <th width="10%">Albums</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>{displayUsers}</tbody>
+              </table>
+              <br></br>
+              <ReactPaginate
+                previousLabel={"Previous"}
+                nextLabel={"Next"}
+                pageCount={pageCount}
+                onPageChange={changePage}
+                containerClassName={"pagination"}
+                activeClassName={"active"}
+              />
+            </div>
           )}
         </main>
       </div>
@@ -48,4 +77,4 @@ const UserPage = () => {
   );
 };
 
-export default UserPage;
+export default UsersPage;
